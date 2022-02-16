09e08a6329e54db36065725fcb286ef41a33194a
root <root@DESKTOP-LN4BGV1.localdomain>
2022-02-16 15:20:39 +0000

    created TDD to build functionality of /api/articles/:article_id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -56,6 +56,52 @@ describe('app()', () => {
               });
         });
      });
+
+     describe('GET /api/articles/:article_id', () => {
+        test('status:200, responds with an article object and it contains correct properties', () => {
+            return request(app)
+              .get('/api/articles/2')
+              .expect(200)
+              .then(({ body }) => {
+                const { article } = body;    // body contains article
+                expect(article).toBeInstanceOf(Object);
+                    expect(article).toEqual(
+                             expect.objectContaining({
+                                        author: expect.any(String),
+                                        title: expect.any(String),
+                                        article_id: expect.any(Number),
+                                        body: expect.any(String),
+                                        topic: expect.any(String),
+                                        created_at: expect.any(String), // should be a date
+                                        votes: expect.any(Number),
+                              })
+                     );
+            })
+            
+        });
+        test('status:400, responds with invalid id message found message for bad id', () => {
+            return request(app)
+              .get('/api/articles/not-an-id')       // id is not valid format
+              .expect(400)
+              .then(({ body: {msg} }) => {
+                expect(msg).toBe('invalid id');
+              });
+        });
+        test('status:404, responds with Article does not exist for -ve id', () => {
+            return request(app)
+              .get('/api/articles/-1')       // -ve id is not an id in table
+              .expect(404)
+              .then(({ body: {msg} }) => {
+                expect(msg).toBe('Article does not exist');
+              });
+        });
+        test('status:404, responds with Article does not exist message for incorrect path', () => {
+            return request(app)
+              .get('/api/articles/99999')          // id is valid but not in table
+              .expect(404)
+              .then(({ body: {msg} }) => {
+                expect(msg).toBe('Article does not exist');
+              });
+        });
+    });
 });
-
-
